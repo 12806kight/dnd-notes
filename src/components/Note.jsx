6a58a8aa70bfc4bfea0a3534933e885472d990a6
Brewer <12806kight@gmail.com>
2023-11-14 14:27:20 -0600

    Basic outline.

---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note(){
+    return( 
+        <div className="note">
+            <h1>This is a note</h1>
+            <p>for my adventures</p>
+        </div>
+
+    );
+}
+
+export default Note;
