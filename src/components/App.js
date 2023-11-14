7a0d5777b917b004a1962736ef2034b66e008c26
Brewer <12806kight@gmail.com>
2023-11-14 16:00:56 -0600

    Handle submit and creating a new note.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note"
 import CreateNote from "./CreateNote"
 
 function App(){
+
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote){
+    setNotes(previousNote =>{
+      return [...previousNote, newNote];
+    })
+  }
+
   return (
     <div>
     <Header/>
-    <CreateNote/>
-    <Note key={1} title="Note title" content="Note content"/>
+    <CreateNote onAdd={addNote}/>
+    {notes.map((i, index) =>{
+      return (
+        <Note
+        key={index}
+        id={index}
+        title={i.title}
+        content={i.content}
+        />
+      )
+    })}
     <Footer/>
     </div>
   );
--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -1,26 +1,35 @@
 import React, {useState} from "react";
 
-function CreateNote(){
-    const [notes, setNotes] = useState({
+function CreateNote(props){
+    const [note, setNote] = useState({
         title : "",
         content: ""
     })
 
     function handleChange(event){
         const {name , value} = event.target;
-        setNotes(previousNotes =>{
+        setNote(previousNotes =>{
             return {
                 ...previousNotes,
                 [name] : value
             }
         })
     }
+
+    function submitNote(event){
+        props.onAdd(note);
+        setNote({
+            title: "",
+            content: ""
+        })
+        event.preventDefault();
+    }
     return(
     <div>
         <form>
-            <input name="title" onChange={handleChange} value={notes.title}></input>
-            <textarea name="content" onChange={handleChange} value={notes.content} rows="3"></textarea>
-            <button>Add</button>
+            <input name="title" onChange={handleChange} value={note.title}></input>
+            <textarea name="content" onChange={handleChange} value={note.content} rows="3"></textarea>
+            <button onClick={submitNote}>Add</button>
         </form>
     </div>
     );
